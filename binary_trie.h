6e40eb33ddafce966420f9f02ae2c7e2843b25e8
kametan0730 <kametan0730@gmail.com>
2022-08-28 20:21:42 +0900

    コメントなど

--- binary_trie.h
+++ binary_trie.h
@@ -7,12 +7,12 @@
 #define IP_BIT_LEN 32
 
 template<typename DATA_TYPE>
-struct binary_trie_node{
-    DATA_TYPE *data;
-    uint32_t depth; // 木の深さ
+struct binary_trie_node{ // 二分トライ木構造のノード
+    DATA_TYPE *data; // 保持するデータ
+    uint32_t depth; // ルートノードからの深さ
     binary_trie_node *parent; // 親ノード
-    binary_trie_node *node_0;
-    binary_trie_node *node_1;
+    binary_trie_node *node_0; // 0側の子ノード
+    binary_trie_node *node_1; // 1側の子ノード
 };
 
 template<typename DATA_TYPE>
@@ -47,8 +47,9 @@ void binary_trie_add(binary_trie_node<DATA_TYPE> *root, uint32_t prefix, uint32_
 template<typename DATA_TYPE>
 DATA_TYPE *binary_trie_search(binary_trie_node<DATA_TYPE> *root, uint32_t prefix){ // 検索
 
-    binary_trie_node<DATA_TYPE> *current = root;
+    binary_trie_node<DATA_TYPE> *current = root; // ルートノードから辿る
 
+    // 検索するIPアドレスと比較して1ビットずつ辿っていく
     for(int i = 1; i <= IP_BIT_LEN; ++i){
         if((prefix >> (IP_BIT_LEN - i)) & 0x01){ // 上からiビット目が1だったら
             if(current->node_1 == nullptr){
@@ -74,9 +75,8 @@ uint32_t locate_prefix(binary_trie_node<DATA_TYPE> *target, binary_trie_node<DAT
             result |= (1 << (32 - len));
         }
         len--;
-        target = target->parent;
+        target = target->parent; // 上にたどっていく
     }
-
     return result;
 }
 
